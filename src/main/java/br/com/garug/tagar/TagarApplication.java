4eacf1811e8e62625be3817d90d2b9c31254f17c
garug <allefff@gmail.com>
2020-06-12 22:46:57 -0300

    Initial Commit

---
+++ src/main/java/br/com/garug/tagar/TagarApplication.java
@@ -0,0 +1,13 @@
+package br.com.garug.tagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/garug/tagar/TagarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.garug.tagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
