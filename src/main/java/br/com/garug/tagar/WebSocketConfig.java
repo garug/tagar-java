994b4419667ae8cbacc49fc9e72bb39d678fc2d3
William Barreto <bsi.william@gmail.com>
2020-06-15 00:11:52 -0300

    Adicionado captura de evento de unsubscribe

--- src/main/java/br/com/garug/tagar/WebSocketConfig.java
+++ src/main/java/br/com/garug/tagar/WebSocketConfig.java
@@ -1,16 +1,14 @@
 package br.com.garug.tagar;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -33,5 +31,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void onSocketDisconnected(SessionDisconnectEvent event) {
         StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
         System.out.println("[Disonnected] " + sha.getSessionId());
+    }
+
+    @EventListener
+    public void onUnsubscribe(SessionUnsubscribeEvent event){
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(event.getMessage());
+        System.out.println("[Unsubscribe] " + sha.getSessionId());
     }
 }
